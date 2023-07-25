5cd3a8fb22fa1edda953be68ce099927b9974846
Dyllan <dyllan.salgado@usach.cl>
2023-07-25 01:13:54 -0400

    Cuando se ingresa a una url inexistente y para que no aparezca error se a añadido al app-routing.module una nueva ruta que redirija al home

--- AngularPractica/src/app/app-routing.module.ts
+++ AngularPractica/src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
 const routes: Routes = [
-  {path: 'dominicode', component: DominicodeComponent}
+  //Estas rutas se leen en orden la ruta '**' siempre debe ir al final
+
+  //Se crea una nueva ruta que muestra el componente dominicode
+  {path: 'dominicode', component: DominicodeComponent},
+  //Ahora para evitar que se muestre el error de consola cuando se ingresa a rutas que no existen se hace lo siguiente
+  {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
 @NgModule({
