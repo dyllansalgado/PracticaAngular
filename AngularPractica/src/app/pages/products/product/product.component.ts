3cab5de63add64c1ecd28a6db25dbc48ad42e855
Dyllan <dyllan.salgado@usach.cl>
2023-07-26 17:52:42 -0400

    La vista de producto se hace con un componente padre e hijo, solo eso ha cambiado

--- AngularPractica/src/app/pages/products/product/product.component.ts
+++ AngularPractica/src/app/pages/products/product/product.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../interfaces/product.interface';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  @Input() product!:Product;
+  constructor(){}
+  ngOnInit(): void{
+
+  }
 
 }
