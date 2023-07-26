f3ac78914772ca28efe3b687761a93b9f75c17cf
Dyllan <dyllan.salgado@usach.cl>
2023-07-26 17:21:24 -0400

    Se muestra la informacion del json en el front,ademas de realizar y utilizar mas componentes en el front y se maquetea el card de productos

--- AngularPractica/src/app/material.module.ts
+++ AngularPractica/src/app/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 //CADA VEZ QUE NECESITE IMPORTAR UN MODULO DE MATERIALS SE INGRESA ACA
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
- exports:[MatToolbarModule]
+    //MATCARDMODULE SE OCUPA EN PRODUCTS, POR LO QUE MaterialModule TAMBIEN SE DEBE IMPORTAR EN prodcuts.module.ts
+ exports:[MatToolbarModule,MatCardModule,MatButtonModule,MatIconModule]
 })
 export class MaterialModule {}
--- AngularPractica/src/app/pages/products/products.component.ts
+++ AngularPractica/src/app/pages/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { ProductsService } from './services/products.service';
 import { tap } from 'rxjs/operators';
+import { Product } from './interfaces/product.interface';
 
 @Component({
   selector: 'app-products',
@@ -8,10 +9,12 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit{
+  products!:Product[];
+
   constructor(private productSvc:ProductsService){}
 
   ngOnInit(): void {
-    this.productSvc.getProducts().pipe(tap( res => console.log(res))).subscribe();
+    this.productSvc.getProducts().pipe(tap((products:Product[])=>this.products =products)).subscribe();
     
   }
 }
